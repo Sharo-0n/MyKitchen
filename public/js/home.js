2cc486317cb508f4cdc6c00cee7b8163f4b66f4f
Shuyuan Ma <shm025@ucsd.edu>
2017-02-14 16:31:17 -0800

    more in order

---
+++ public/js/home.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+
+	//$(".click-listener").click(function(event) {
+	//	event.preventDefault();
+		
+	//})
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+}
+
