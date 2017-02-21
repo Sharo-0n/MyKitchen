d7534164f01f1084e975a61f4c64f654d6f1c999
Dominic Spencer <dominicspencer96@gmail.com>
2017-02-20 17:41:46 -0800

    message

--- public/js/home.js
+++ public/js/home.js
@@ -21,7 +21,7 @@ $( ".cross" ).hide();
 $( ".menu" ).hide();
 $( ".hamburger" ).click(function() {
 $( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).show();
+$( ".cross" ).show();8
 });
 });
 
@@ -52,4 +52,7 @@ window.onclick = function(event) {
     }
   }
 }
-
+/* function to toggle the locations of buttons relative to window size */
+/*function windowSize() {
+  var w= 
+}*/
